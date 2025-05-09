26eb551f81044548e4c7471f3e53419cae914d0c
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2025-05-09 20:45:23 +0300

    Update

--- src/app/pages/our-servcies/service-details/service-details.component.ts
+++ src/app/pages/our-servcies/service-details/service-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,7 +9,9 @@ import { ActivatedRoute } from '@angular/router';
                   '../../../../assets/css/general.css',
                   '../../../../assets/css/sections/contactSection.css',
                   '../../../../assets/css/sections/ourServiceFlow.css'
-  ]
+  ],
+  encapsulation: ViewEncapsulation.None,
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ServiceDetailsComponent implements OnInit{
   newsId: string;
